23fe784f6361cfc3ee9d8bd7be54dc462f6e42c6
Marwan Sarieddine <sarieddine.marwan@gmail.com>
2014-12-29 23:29:07 -0500

    update keyword model

--- app/models/keyword.rb
+++ app/models/keyword.rb
@@ -15,7 +15,7 @@ class Keyword < ActiveRecord::Base
 			new_tweet.text=tweet.text
 
 			new_tweet.user_uid= tweet.user.id
-			new_tweet.user_name=tweet.user.user_name
+			new_tweet.user_name=tweet.user.screen_name
 			new_tweet.user_screen_name=tweet.user.screen_name
 			new_tweet.user_image_url=tweet.user.profile_image_url.to_s
 
